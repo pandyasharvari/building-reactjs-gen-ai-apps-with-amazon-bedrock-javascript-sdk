233efa9993aca5b8a4cfeeafe03640247c717ce7
pandyasharvari <157615336+pandyasharvari@users.noreply.github.com>
2024-05-14 23:14:37 +0100

    Update Menu.jsx

--- reactjs-gen-ai-apps/src/Menu.jsx
+++ reactjs-gen-ai-apps/src/Menu.jsx
@@ -28,7 +28,7 @@ const Menu = ({ signOut, groups, ...user }) => {
 
       identity={{
         onFollow: (() => { navigate("/") }),
-        title: <div className='title'>Amazon Bedrock Javascript</div>,
+        title: <div className='title'>HealthScribe QnA</div>,
 
       }}
       utilities={[
@@ -46,31 +46,7 @@ const Menu = ({ signOut, groups, ...user }) => {
           href: "https://github.com/build-on-aws/building-reactjs-gen-ai-apps-with-amazon-bedrock-javascript-sdk", 
           external: true, externalIconAriaLabel: " (opens in a new tab)"
         },
-        {
-          type: "menu-dropdown",
-          text: "Authors",
-          onItemClick: ((e) => { itemClick(e) }),
-          iconName: "user-profile",
-          items: [
-            {
-              id: "Eli",
-              text: "Elizabeth Fuentes Leone",
-              href: "https://www.linkedin.com/in/lizfue/",
-              external: true,
-              externalIconAriaLabel:
-                " (opens in new tab)"
-            },
-            {
-              id: "Kike",
-              text: "Enrique Rodríguez",
-              href: "https://www.linkedin.com/in/enriquerodriguezgarrido/",
-              external: true,
-              externalIconAriaLabel:
-                " (opens in new tab)"
-            },
-          ]
-
-        },
+     
         {
           type: "menu-dropdown",
           text: "You",
