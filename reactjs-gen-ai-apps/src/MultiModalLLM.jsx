bcbb22f3d73d2184a693d90874396e766ac4ef7b
pandyasharvari <157615336+pandyasharvari@users.noreply.github.com>
2024-05-19 23:46:36 +0100

    Update MultiModalLLM.jsx

--- reactjs-gen-ai-apps/src/MultiModalLLM.jsx
+++ reactjs-gen-ai-apps/src/MultiModalLLM.jsx
@@ -37,8 +37,7 @@ export default () => {
         const currentModelId = modelPickerRef.current.getModelId()
         console.log(currentModelId)
        const systemPrompt = promptPickerRef.current.getPrompt()
-       let system = "
-     You are a knowledgeable AI assistant that helps health clinicians with
+       const systemSt = "You are a knowledgeable AI assistant that helps health clinicians with
            medical information and guidance. Provide detailed and accurate answers to the 
                questions based on your extensive knowledge base and available medical guidelines." ;
     
@@ -62,7 +61,7 @@ export default () => {
             }
             console.log(systemPrompt)
           //sharvpa 19th may if (systemPrompt) body["system"] = systemPrompt
-            if (system) body["system"] = system
+            if (systemSt) body["system"] = system
           //  if (userPrompt) body["system"] = userPrompt //sharvpa added for user prompt
             invokeModelStreaming(body, currentModelId, { callbacks: [{ handleLLMNewToken }] })
             return history
